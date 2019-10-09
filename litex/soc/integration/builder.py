8002f5f64e390baf2c70c7655d8bf0fb26773b6f
Stefan Adams <stefan.adams@vipcomag.de>
2019-10-09 13:19:21 +0200

    fixed: check for "rocket64"

--- litex/soc/integration/builder.py
+++ litex/soc/integration/builder.py
@@ -123,7 +123,7 @@ class Builder:
                         self.soc.sdram.controller.settings.phy,
                         self.soc.sdram.controller.settings.timing))
 
-        if self.soc.cpu_type == "rocket2":
+        if self.soc.cpu_type == "rocket64":
             if hasattr(self.soc, "get_dts"):
                 dts = self.soc.get_dts()
             else:
