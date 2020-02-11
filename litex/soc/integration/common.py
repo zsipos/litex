72ed26dd6c99b75f1e5ef995e9e9a8884d4f28c2
Stefan Adams <stefan.adams@vipcomag.de>
2020-02-11 08:29:55 +0100

    different mem_decoder for rocket is obsolete

--- litex/soc/integration/common.py
+++ litex/soc/integration/common.py
@@ -19,13 +19,6 @@ def mem_decoder(address, size=0x10000000):
     size    >>= 2 # bytes to words aligned
     return lambda a: (a[log2_int(size):] == (address >> log2_int(size)))
 
-def mem_decoder_rocket(address, size=0x10000000):
-    size = 2**log2_int(size, False)
-    assert (address & (size - 1)) == 0
-    address >>= 2 # bytes to words aligned
-    size    >>= 2 # bytes to words aligned
-    return lambda a: (a[log2_int(size):] == (address >> log2_int(size)))
-
 def get_version(with_time=True):
     if with_time:
         return datetime.datetime.fromtimestamp(
